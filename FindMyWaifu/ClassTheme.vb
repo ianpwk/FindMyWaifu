9e01ea0e7307e7478aeadf3b781963d6991c94d9
Ian Ardiansyah <ian.ian.wold76@gmail.com>
2019-06-27 21:46:43 +0700

    Add white button icon after change to dark

--- FindMyWaifu/ClassTheme.vb
+++ FindMyWaifu/ClassTheme.vb
@@ -11,12 +11,18 @@
         MainFrm.ToolStrip2.BackColor = Color.FromArgb(224, 224, 224)
         MainFrm.ToolStrip2.ForeColor = Color.Black
 
+        MainFrm.Conn_btn.Image = My.Resources.connect
+        MainFrm.ToolStripSplitButton1.Image = My.Resources.menu
+
         MainFrm.BackColor = Color.White
         MainFrm.ForeColor = Color.Black
     End Sub
     Private Sub ColorDark()
         MainFrm.ToolStrip2.BackColor = Color.DimGray
         MainFrm.ToolStrip2.ForeColor = Color.White
+
+        MainFrm.Conn_btn.Image = My.Resources.white_connect
+        MainFrm.ToolStripSplitButton1.Image = My.Resources.white_menu
 
         MainFrm.BackColor = Color.Black
         MainFrm.ForeColor = Color.White
--- FindMyWaifu/My Project/Resources.Designer.vb
+++ FindMyWaifu/My Project/Resources.Designer.vb
@@ -119,5 +119,25 @@ Namespace My.Resources
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property white_connect() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("white_connect", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property white_menu() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("white_menu", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
     End Module
 End Namespace
